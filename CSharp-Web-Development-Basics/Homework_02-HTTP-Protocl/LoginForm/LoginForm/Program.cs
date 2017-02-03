f59959b0fc69427fbc6ac53432135334e4f10950
Magdalena Petrova <magik70@abv.bg>
2017-02-03 10:05:02 +0200

    LoginForm
    using Apache cgi-bin

---
+++ CSharp-Web-Development-Basics/Homework_02-HTTP-Protocl/LoginForm/LoginForm/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace LoginForm
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Content-Type: text/html\r\n");
+            string html = File.ReadAllText("../htdocs/LoginForm/loginForm.html");
+            Console.WriteLine(html);
+
+            string[] inputContent = Console.ReadLine().Split('&');
+            Console.WriteLine("\r\n<section id=\"user-info\">\r\n");
+
+            string user = "";
+            foreach (var item in inputContent)
+            {
+                string key = item.Split('=')[0];
+                string itemValue = item.Split('=')[1];
+                if (String.IsNullOrEmpty(itemValue))
+                {
+                    string errorMsg = $"Invalid input! {key} can not by empty!";
+                    Console.WriteLine($"<p id=\"errorMsg\">{errorMsg}</p>\r\n");
+                    break;
+                }
+                else
+                {
+                    user += WebUtility.HtmlEncode(itemValue) + ";";
+                }
+            }
+
+            Console.WriteLine($"<p>Hi {user.Split(';')[0]}, your password is {user.Split(';')[1]}</p>\r\n");
+            Console.WriteLine("</section>");
+        }
+    }
+}
